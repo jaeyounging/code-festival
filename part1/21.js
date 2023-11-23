bd5e7696d1023095541f0b487c5ad79adeb4bd14
Jaeyoung Lee <5061yo@naver.com>
2023-11-24 00:17:54 +0900

    # 문제21 : set은 어떻게 만드나요?

---
+++ part1/21.js
@@ -0,0 +1,16 @@
+// # 문제21 : set은 어떻게 만드나요?
+
+// 다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+// 1)  var x = {1, 2, 3, 5, 6, 7};
+// 2)  var x = {};
+// 3)  var x = new Set('javascript');
+// 4)  var x = new Set(range(5));
+// 5)  var x = new Set();
+
+/* 
+set : 중복되지 않는 데이터를 저장하는 데이터 구조
+set.add() : 데이터 추가
+set.delete() : 데이터 삭제
+set.has() : 매개변수의 값이 있는지 없는지 확인
+*/
